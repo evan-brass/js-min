89a3fa1b636936da190994c710ca5c01279e349e
Evan Brass <evan-brass@pm.me>
2019-03-24 16:49:10 -0700

    First thought for live data

---
+++ live-data.mjs
@@ -0,0 +1,35 @@
+
+export default class LiveData {
+	constructor() {
+		this.waiters = [];
+		this.animationFrame = false;
+	}
+	set value(newValue) {
+		this._value = newValue;
+		if (!this.animationFrame) {
+			this.animationFrame = requestAnimationFrame(() => {
+				for (const callback of this.waiters) {
+					callback(newValue);
+				}
+				this.waiters.length = 0;
+				this.animationFrame = false;
+			});
+		}
+	}
+	get value() {
+		return this._value;
+	}
+	then(callback) {
+		this.waiters.push(callback);
+	}
+	async *[Symbol.asyncIterator]() {
+		while (true) {
+			const lastIssued = this._value;
+			yield lastIssued;
+			if (this._value == lastIssued) {
+				// If we're caught up then wait for an update
+				await this;
+			}
+		}
+	}
+}
