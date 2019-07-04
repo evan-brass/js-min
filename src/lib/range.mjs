47315d30ff3d5202b7daeb4614cf9195f1f08fc5
Evan Brass <evan-brass@protonmail.com>
2019-07-04 16:10:18 -0700

    Allow negative steps in range constructor (decrement instead of increment)

--- src/lib/range.mjs
+++ src/lib/range.mjs
@@ -1,5 +1,5 @@
 export default function* range(start, end, step = 1) {
-    for (let i = start; i < end; i += step) {
+    for (let i = start; (step > 0) ? i < end : i > end; i += step) {
         yield i;
     }
 }
