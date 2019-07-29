61fa2e63dd14a9dec824bc2ac15dcd28391c7ae6
Evan Brass <evan-brass@protonmail.com>
2019-07-29 10:49:47 -0700

    Share stylesheets for styles without any parts

--- src/users/css.mjs
+++ src/users/css.mjs
@@ -2,39 +2,50 @@ import User from './user.mjs';
 import StylePart from '../parts/style-part.mjs';
 import { expression2user, verifyUser } from './common.mjs';
 
+const styleSheetCache = new Map(); // Could be a weakmap
 
 export default function css(strings, ...expressions) {
 	return {
 		get [User]() {return this;},
 		acceptTypes: ['node'],
 		bind(part) {
-			this.users = expressions.map(expression2user);
 			this.root = part.element.getRootNode();
-			this.stylesheet = new CSSStyleSheet({});
-			const shared = [];
 			this.parts = [];
-			let strings_i;
-			for (strings_i = 0; strings_i < (strings.length - 1); ++strings_i) {
+			if (strings.length == 1) {
+				if (!styleSheetCache.has(strings)) {
+					this.stylesheet = new CSSStyleSheet({});
+					this.stylesheet.replace(Array.prototype.join.call(strings, ''));
+					styleSheetCache.set(strings, this.stylesheet);
+				} else {
+					this.stylesheet = styleSheetCache.get(strings);
+				}
+			} else {
+				this.stylesheet = new CSSStyleSheet({});
+				this.users = expressions.map(expression2user);
+				const shared = [];
+				let strings_i;
+				for (strings_i = 0; strings_i < (strings.length - 1); ++strings_i) {
+					shared.push(strings[strings_i]);
+					shared.push('');
+					this.parts.push(new StylePart(part.element, this.stylesheet, shared, shared.length - 1));
+				}
 				shared.push(strings[strings_i]);
-				shared.push('');
-				this.parts.push(new StylePart(part.element, this.stylesheet, shared, shared.length - 1));
-			}
-			shared.push(strings[strings_i]);
-			if (this.parts.length !== this.users.length) {
-				throw new Error("There aren't the proper number of expressions for this literal.");
-			}
-			this.stylesheet.replace(shared.join(''));
-			for (let i = 0; i < this.parts.length; ++i) {
-				const part = this.parts[i];
-				const user = this.users[i];
-				verifyUser(user, part);
-				user.bind(part);
+				if (this.parts.length !== this.users.length) {
+					throw new Error("There aren't the proper number of expressions for this literal.");
+				}
+				this.stylesheet.replace(shared.join(''));
+				for (let i = 0; i < this.parts.length; ++i) {
+					const part = this.parts[i];
+					const user = this.users[i];
+					verifyUser(user, part);
+					user.bind(part);
+				}
 			}
 			const newStyleSheets = Array.from(this.root.adoptedStyleSheets);
 			newStyleSheets.push(this.stylesheet);
 			this.root.adoptedStyleSheets = newStyleSheets;
 		},
-		unbind(part) {
+		unbind(_part) {
 			const newStyleSheets = Array.from(this.root.adoptedStyleSheets);
 			newStyleSheets.splice(newStyleSheets.indexOf(this.stylesheet), 1);
 			this.root.adoptedStyleSheets = newStyleSheets;
