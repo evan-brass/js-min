a6ae3fa72d668b23e2b39873d9d1435df3938960
Evan Brass <evan-brass@protonmail.com>
2019-07-29 15:07:39 -0700

    modify user instead of allocating sub user

--- src/users/array-handle.mjs
+++ src/users/array-handle.mjs
@@ -5,20 +5,28 @@ import ALLTYPES from '../parts/all-types.mjs';
 function joinHandle(expressions, part) {
 	const shared = new Array(users.length).fill('');
 	const users = Array.from(expressions).map(expression2user);
+	const fakeParts = [];
 	for (let i = 0; i < users.length; ++i) {
 		const fakePart = {
+			// This makes me wonder what a part even is.  Atribute parts don't have update and here I'm creating a fake style or attribute-value part.  Style parts and these fake ones don't even have element attributes.
 			type: part.type,
 			update(newValue) {
 				shared[i] = newValue;
-				part.update(shared.join(' ')); // I'm pretty sure that both style and attribute-values can use a space between thier array values.
+				part.update(shared.join(' ')); // I'm pretty sure that both style and attribute-values can use a space between their array values.
 			},
 			clear() { this.update(''); }
 		}
+		fakeParts.push(fakePart);
 		const user = users[i];
 		verifyUser(user, fakePart);
 	}
-	return {
-		unbind(part) { part.clear(); }
+	return part => {
+		part.unbind();
+		for (let i = 0; i < users.length; ++i) {
+			const user = users[i];
+			const fakePart = fakeParts[i];
+			user.unbind(fakePart);
+		}
 	};
 }
 
@@ -28,11 +36,9 @@ function shareHandle(expressions, part) {
 		verifyUser(user, part);
 		user.bind(part);
 	}
-	return {
-		unbind(part) {
-			for (const user of users) {
-				user.unbind(part);
-			}
+	return part => {
+		for (const user of users) {
+			user.unbind(part);
 		}
 	};
 }
@@ -47,10 +53,10 @@ export default function arrayHandle(expression) {
 				case 'style':
 					// Should probably use insertRule and deleteRule instead of array joining, but...
 				case 'attribute-value':
-					this.internal = joinHandle(expression, part);
+					this.unbind = joinHandle(expression, part);
 					break;
 				case 'attribute':
-					this.internal = shareHandle(expressions, part);
+					this.unbind = shareHandle(expression, part);
 					break;
 				case 'node':
 					// TODO: Use something like Array-Instance
@@ -58,8 +64,8 @@ export default function arrayHandle(expression) {
 					throw new Error("A default array user hasn't been defined for that type of part");
 			}
 		},
-		unbind(part) {
-			this.internal.unbind(part);
+		unbind(_part) { 
+			throw new Error('unbind should have been overridden or bind was not called before unbind'); 
 		}
 	}
 }
