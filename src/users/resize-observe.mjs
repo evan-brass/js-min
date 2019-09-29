0a47e9599ba2aa659fe839eb8e24417f2d6dd114
Evan Brass <evan-brass@protonmail.com>
2019-09-28 21:22:59 -0700

    Add options for resize observer

--- src/users/resize-observe.mjs
+++ src/users/resize-observe.mjs
@@ -1,13 +1,13 @@
 import User from './user.mjs';
 
-export default function resizeObserve(observer) {
+export default function resizeObserve(observer, options = {}) {
 	return {
 		acceptTypes: new Set(["attribute"]),
 		bind(part) {
-			observer.observe(part.element);
+			observer.observe(part.element, options);
 		},
 		unbind(part) {
-			observer.unobserve(part.element);
+			observer.unobserve(part.element, options);
 		},
 		get [User]() { return this; }
 	}
