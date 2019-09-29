65e17d31b205e98a70b5b775d10c9e4507b70982
Evan Brass <evan-brass@protonmail.com>
2019-09-28 21:22:47 -0700

    Make abort errors consistent

--- src/lib/delay.mjs
+++ src/lib/delay.mjs
@@ -1,10 +1,13 @@
-export default function delay(ms, abortSignal = false) {
+import abortError from '../cancellation/abort-error.mjs';
+
+export default function delay(ms, signal = undefined) {
 	return new Promise((resolve, reject) => {
 		const handle = setTimeout(resolve, ms);
-		if (abortSignal) {
-			abortSignal.addEventListener('abort', _ => {
+		// TODO: extract the if (signal !== undefined) {signal.addEventListener('abort', /*  CODE */)} into a helper method.
+		if (signal !== undefined) {
+			signal.addEventListener('abort', _ => {
 				clearTimeout(handle);
-				reject(new DOMException('Signal Aborted.', 'AbortError'));
+				reject(abortError());
 			});
 		}
 	});
