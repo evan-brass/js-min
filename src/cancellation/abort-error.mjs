3772c8ef6cbea806cf7cbac37b30a2ac60964dcf
brackish <evan-brass@protonmail.com>
2020-01-21 11:52:58 -0800

    Fix Types in cancellation

--- src/cancellation/abort-error.mjs
+++ src/cancellation/abort-error.mjs
@@ -1,8 +1,14 @@
 // @flow
+/*::
+declare class DOMException {
+	err: string;
+	constructor(message?: string, name?: string): DOMException
+}
+*/
 
 export default function abortError() {
 	// TODO: Decide upon a uniform message for abort errors
-	return new DOMException('Signal Aborted.', 'AbortError')
+	return new DOMException('Signal Aborted.', 'AbortError');
 	// return new DOMException('Operation Aborted.', 'AbortError');
 	// return new DOMException('User aborted.', 'AbortError');
 }
--- src/cancellation/wrap-signal.mjs
+++ src/cancellation/wrap-signal.mjs
@@ -1,18 +1,21 @@
 // @flow
 
+import AbortError from './abort-error.mjs';
+
 // TODO: Add reference counting / something to automatically abort an operation if it is no longer needed.
 
-export default function wrapSignal(signal) {
-	const signalPromise = new Promise((_, reject) => {
-		signal.addEventListener('abort', _ => {
-			reject(new DOMException('Signal was aborted', 'AbortError'));
-		});
+export default function wrapSignal(signal/*:AbortSignal*/) {
+	// This promise will never resolve.
+	const signalPromise/*:Promise<void>*/ = new Promise((_resolve, reject) => {
+		signal.addEventListener('abort', reject.bind(null, AbortError()));
 	});
-	return function wrap(promise) {
+	return function wrap/*flow-include <T>*/(promise/*: Promise<T>*/)/*:Promise<T>*/ {
+		// Safe to cast because signalPromise will never resolve.
+		const castedSignalPromise = /*flow-include ((*/signalPromise/*: any): Promise<T>)*/
 		if (signal.aborted) {
-			return signalPromise;
+			return castedSignalPromise;
 		} else {
-			return Promise.race([signalPromise, promise]);
+			return Promise.race([castedSignalPromise, promise]);
 		}
 	};
 }
