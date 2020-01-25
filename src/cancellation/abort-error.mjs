2e8adc90e9d2c9a8a49c514a82c4652ea0e6d08e
brackish <evan-brass@protonmail.com>
2020-01-25 10:29:05 -0800

    Cleanup
    Delete unused Code + comments + Tabs

--- src/cancellation/abort-error.mjs
+++ src/cancellation/abort-error.mjs
@@ -1,6 +1,6 @@
 export default function abortError() {
 	// TODO: Decide upon a uniform message for abort errors
-	return new DOMException('Signal Aborted.', 'AbortError')
+	return new DOMException('Signal Aborted.', 'AbortError');
 	// return new DOMException('Operation Aborted.', 'AbortError');
 	// return new DOMException('User aborted.', 'AbortError');
 }
