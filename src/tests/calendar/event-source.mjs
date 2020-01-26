4ebdd6dd640beb0a8684332ded5fe41731e8e833
brackish <evan-brass@protonmail.com>
2020-01-26 10:01:16 -0800

    Make getEvents cancelable

--- src/tests/calendar/event-source.mjs
+++ src/tests/calendar/event-source.mjs
@@ -1,7 +1,7 @@
 import Trait from '../../lib/trait.mjs';
 
 export default new Trait("EventSource", {
-	async getEvents(_interval) {
+	async getEvents(_interval, _abortSignal) {
 		throw new Error('No default implementation for getEvents.');
 	}
 });
