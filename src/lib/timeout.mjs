a5097d008615bf64e6b288ee40ccd50f797e785e
brackish <evan-brass@protonmail.com>
2020-03-20 15:20:44 -0700

    New tools from web3.0-test

---
+++ src/lib/timeout.mjs
@@ -0,0 +1,11 @@
+export default function timeout(time_limit, signal = false) {
+    const controller = new AbortController();
+    function abort() {
+        controller.abort();
+    }
+    if (signal) {
+        signal.addEventListener('abort', abort);
+    }
+    setTimeout(abort, time_limit);
+    return controller.signal;
+}
