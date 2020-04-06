c83aaea684a6a6de03b71348df806b2a5a6c4ee2
Evan Brass <evan-brass@protonmail.com>
2020-04-06 10:11:03 -0700

    seperate calendar css

--- examples/calendar/calendar-basic.mjs
+++ examples/calendar/calendar-basic.mjs
@@ -1,6 +1,5 @@
 import Base from '../../src/custom-elements/base.mjs';
 import html from '../../src/templating/html.mjs';
-import css from '../../src/templating/css.mjs';
 import mount from '../../src/templating/mount.mjs';
 import NEVER from '../../src/lib/never.mjs';
 import wrapSignal from '../../src/cancellation/wrap-signal.mjs';
@@ -16,6 +15,8 @@ import ref from '../../src/templating/users/ref.mjs';
 import arrow_nav from '../../src/templating/users/arrow-nav.mjs';
 import component from '../../src/templating/users/component.mjs';
 
+import calendar_basic_css from './calendar-basic.css.mjs';
+
 export default class CalendarBasic extends Base {
 	constructor() {
 		super();
@@ -26,74 +27,7 @@ export default class CalendarBasic extends Base {
 
 	// TODO: Replace thise styles thing with CSS modules
 	get styles() {
-		return css`:host {
-			direction: auto;
-			display: grid;
-			grid-template-columns: repeat(7, minmax(0, 1fr));
-			grid-template-rows: auto auto repeat(6, auto minmax(1em, auto) minmax(1em, auto) minmax(1em, auto) minmax(1em, auto) minmax(1em, auto));
-			border: 1px solid #aaa;
-			border-bottom: 0;
-			/*
-				overflow-y: auto;
-				overflow-x: hidden;
-			*/
-			/* display: layout(blocklike); */
-		}
-		header {
-			display: flex;
-			align-items: baseline;
-			grid-column: auto / span 7;
-		}
-		.title {
-			order: 0;
-		}
-		.displayed-month {
-			order: 1;
-		}
-		.title, .displayed-month {
-			margin: 0;
-		}
-		.displayed-month {
-
-		}
-		.title {
-			flex-grow: 1;
-		}
-		.weekdays {
-			/* Prevent languages with multi word weekday names (like hebrew) from wrapping before attempting to use a smaller weekday name format: */
-			white-space: nowrap;
-			display: contents;
-			text-align: center;
-		}
-		.weekday {
-
-		}
-		.cells {
-			display: contents;
-		}
-		.cell {
-			/* I'm using negative margins to make colloring the outline of an element easier. TODO: Replace with a paint worklet? */
-			box-sizing: border-box;
-			grid-row: auto / span 6;
-			border: 1px solid #aaa;
-			margin: -1px -1px 0 0;
-			outline: 0;
-		}
-		.cell.basis {
-			z-index: 5;
-			border-color: lime;
-		}
-		.cell:dir(ltr):nth-of-type(7n - 6),
-		.cell:dir(rtl):nth-of-type(7n),
-		/* Sadly, dir() isn't supported even in Canary so I need a js fallback that uses getComputedStyles(el).direction and applies a css class. */
-		.ltr .cell:nth-of-type(7n - 6),
-		.rtl .cell:nth-of-type(7n) {
-			margin-left: -1px;
-		}
-		.cell:not(.in-month) {
-			background-color: #ddd;
-		}
-		`;
+		return calendar_basic_css;
 	}
 	// I would like to convert this whole LiveData + getter + setter + (attributeChangedCallback?) into a decorator setup once somebody starts supporting them. These also need to delete any attributes that have already been added by a framework before the element was upgraded + call the setter with the value they set.
 	_basis = new LiveData(DateTime.local())
