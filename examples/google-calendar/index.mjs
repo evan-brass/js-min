09e852c16bbb85d28e89f5272a50597c120d0be0
Evan Brass <evan-brass@protonmail.com>
2020-06-20 14:18:14 -0700

    List user's calendars

--- examples/google-calendar/index.mjs
+++ examples/google-calendar/index.mjs
@@ -19,6 +19,23 @@ import LiveData from '../../src/reactivity/live-data.mjs';
 // }
 // document.body.appendChild(counter());
 
+function str(val) {
+	return function(el) {
+		el.replaceWith(new Text(val));
+	}
+}
+
+function arr(arr) {
+	return function(el) {
+		for (const item of arr) {
+			const comment = new Comment();
+			el.parentNode.insertBefore(comment, el);
+			item(comment);
+		}
+		el.remove();
+	}
+}
+
 function single_btn(text = "") {
 	let btn;
 	const prom = new Promise(res => {
@@ -47,29 +64,67 @@ const scopes = [
 	'https://www.googleapis.com/auth/calendar.settings.readonly'
 ];
 
+// 
+const GC_endpoint_base = 'https://www.googleapis.com/calendar/v3';
+
 const temp = new Comment();
 document.body.appendChild(temp);
 sink((async function*() {
-	const get_token = new Promise((resolve, reject) => {
-		const channel = new BroadcastChannel('google-auth');
-		channel.onmessage = e => {
-			if (e.data) {
-				resolve(e.data);
-			} else {
-				reject(new Error('Oauth-catch got an error.'));
-			}
-		};
+	let token = window.localStorage.getItem('google-token');
+	if (token != undefined) {
+		token = JSON.parse(token);
+		if (token.valid_until < Date.now()) {
+			token = undefined;
+		}
+	}
+	if (token == undefined) {
+		// Get a token
+		const get_token = new Promise((resolve, reject) => {
+			const channel = new BroadcastChannel('google-auth');
+			channel.onmessage = e => {
+				if (e.data) {
+					resolve(e.data);
+				} else {
+					reject(new Error('Oauth-catch got an error.'));
+				}
+			};
+		});
+	
+		// Get user click:
+		const login_btn = single_btn('Click to Login');
+		yield html`Beginning OAuth 2.0 request with Google.  ${login_btn}`;
+		await login_btn;
+		window.open(`https://accounts.google.com/o/oauth2/v2/auth?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=token&scope=${scopes.join(' ')}`);
+	
+		// Wait for the token to be received:
+		yield html`Waiting for auth token from redirect page...`;
+		token = await get_token;
+	}
+
+	const GC_headers = new Headers({
+		'Authorization': `${token.token_type} ${token.access_token}`
+	});
+	
+
+	// Fetch the calendars in the user's calendarList
+	yield html`Got token. Fetching list of user's calendars...`;
+
+	let list_response = await fetch(GC_endpoint_base + '/users/me/calendarList', {
+		method: 'GET',
+		headers: GC_headers
 	});
 
-	// Get user click:
-	const login_btn = single_btn('Click to Login');
-	yield html`Beginning OAuth 2.0 request with Google.  ${login_btn}`;
-	await login_btn;
-	window.open(`https://accounts.google.com/o/oauth2/v2/auth?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=token&scope=${scopes.join(' ')}`);
+	if (!list_response.ok) {
+		yield html`Somethin bad happen...`;
+		return;
+	}
 
-	// Wait for the token to be received:
-	yield html`Waiting for auth token from redirect page...`;
-	const received_token = await get_token;
+	list_response = await list_response.json();
+	const calendars = list_response.items;
 
-	yield html`Got token! ${JSON.stringify(received_token)}`;
+	yield html`Got calendar list: <ul>
+		${arr(calendars.map(cal => sink((async function*(){
+			yield html`<li><span ${el => el.style.color = cal.backgroundColor}>${str(cal.summary)}</span></li>`;
+		})())))}
+	</ul>`;
 })())(temp);
