75df0a39020b9f278bf481161780ed58086c1234
Evan Brass <evan-brass@protonmail.com>
2020-06-22 22:51:45 -0700

    Migrate counter-widget to template-v2

--- examples/counter-widget/index.mjs
+++ examples/counter-widget/index.mjs
@@ -1,13 +1,12 @@
-import html from '../../src/templating/html.mjs';
-import mount from '../../src/templating/mount.mjs';
+import html from '../../src/template-v2/html.mjs';
 import LiveData from '../../src/reactivity/live-data.mjs';
-import on from '../../src/templating/users/on.mjs';
+import on from '../../src/template-v2/on.mjs';
 
 const count = new LiveData();
 count.value = 10;
 
-mount(html`
+document.body.appendChild(html`
 	<button ${on('click', () => count.value -= 1)}>-</button>
 	${count}
 	<button ${on('click', () => count.value += 1)}>+</button>
-`, document.body);
+`);
--- src/template-v2/on.mjs
+++ src/template-v2/on.mjs
@@ -1,5 +1,6 @@
 export default function on(event, handler, options = {}) {
-	return function (el) {
+	return function (el, part_kind, _recursive_handler) {
+		if (part_kind !== 'attribute') throw new Error('On can only be used with attribute kinds of parts.');
 		el.addEventListener(event, handler, options);
 	}
 }
