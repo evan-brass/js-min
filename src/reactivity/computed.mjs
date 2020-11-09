4ee0f10e7993eb7ec32d1c69448d9a1eb9f086ca
Evan <evan-brass@protonmail.com>
2020-11-09 15:58:50 -0800

    _

--- src/reactivity/computed.mjs
+++ src/reactivity/computed.mjs
@@ -1,18 +1,28 @@
 import { aquire_waiters, queue_waiters } from "./context.mjs";
 import use_now from './use.mjs';
 
-const UnInit = Symbol("Values hasn't been created yet.");
+const UnInit = Symbol("This computed's value hasn't been created yet.");
 
-export default function single(calc_func) {
+export default function computed(calc_func, did_change = (a, b) => a !== b) {
 	let value = UnInit;
 	let waiters = new Set();
 	return {
 		get value() {
 			if (value === UnInit) {
 				use_now(() => {
-					value = calc_func();
-
-					waiters = queue_waiters(waiters);
+					// Only recompute if there's people waiting for our value.
+					if (waiters.size()) {
+						const old = value;
+					
+						value = calc_func();
+						
+						// Only update our waiters if our value has changed.
+						if (did_change(old, value)) {
+							waiters = queue_waiters(waiters);
+						}
+					} else {
+						value = UnInit;
+					}
 				});
 			}
 
